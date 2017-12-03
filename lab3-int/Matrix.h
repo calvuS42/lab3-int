7115f14344c6123146f7422259a49b04ba6d0635
calvuS42 <olegka1302@gmail.com>
2017-12-03 19:26:56 +0200

    oper +-

--- lab3-int/Matrix.h
+++ lab3-int/Matrix.h
@@ -5,11 +5,16 @@ class Matrix
 private:
 	int sizeColums;
 	int sizeRows;
-	Type *Matrix;
+	Type Matrix[sizeColums][sizeRows];
 public:
 	Matrix();
 	Matrix(Matrix & A);
-
+	Matrix(int, int);
+	Matrix operator +(Matrix A);
+	Matrix operator -(Matrix A);
+	Matrix operator *(Matrix A);
+	Matrix operator /(Matrix A);
+	Matrix operator ~(Matrix A);
 };
 
 template<typename Type>
@@ -26,4 +31,60 @@ inline Matrix<Type>::Matrix(Matrix & A)
 	sizeColums = A.sizeColums;
 	sizeRows = A.sizeRows;
 	Matrix = A.Matrix;
+}
+
+template<typename Type>
+inline Matrix<Type>::Matrix(int a, int b)
+{
+	sizeColums = a;
+	sizeRows = b;
+	Matrix = new Type[a][b];
+}
+
+template<typename Type>
+inline Matrix Matrix<Type>::operator+(Matrix A)
+{
+	if (sizeColums == A.sizeColums && sizeRows == A.sizeRows) {
+		Matrix b(sizeColums, sizeRows);
+		for (int i = 0; i < sizeColums; i++) {
+			for (int j = 0; j < sizeRows; j++) {
+				b.Matrix[i][j] = Matrix[i][j] + A.Matrix[i][j];
+			}
+		}
+		return b;
+	}
+	else return;
+}
+
+template<typename Type>
+inline Matrix Matrix<Type>::operator-(Matrix A)
+{
+	if (sizeColums == A.sizeColums && sizeRows == A.sizeRows) {
+		Matrix b(sizeColums, sizeRows);
+		for (int i = 0; i < sizeColums; i++) {
+			for (int j = 0; j < sizeRows; j++) {
+				b.Matrix[i][j] = Matrix[i][j] - A.Matrix[i][j];
+			}
+		}
+		return b;
+	}
+	else return;
+}
+
+template<typename Type>
+inline Matrix Matrix<Type>::operator*(Matrix A)
+{
+	return Matrix();
+}
+
+template<typename Type>
+inline Matrix Matrix<Type>::operator/(Matrix A)
+{
+	return Matrix();
+}
+
+template<typename Type>
+inline Matrix Matrix<Type>::operator~(Matrix A)
+{
+	return Matrix();
 }
